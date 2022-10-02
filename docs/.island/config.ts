a241fce3d2993a5d248d01f0077673c803f6f4b2
liuqh <528851597@qq.com>
2022-10-02 20:08:08 +0800

    feat: first commit

---
+++ docs/.island/config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from 'islandjs'
+
+export default defineConfig({
+  title: 'my-site',
+  themeConfig: {
+    lang: 'zh',
+    locales: {
+      '/zh/': {
+        lang: 'zh',
+        label: '简体中文',
+        selectText: '语言',
+        ariaLabel: '语言',
+        lastUpdatedText: '上次更新',
+        title: 'Island.js',
+        outlineTitle: '目录',
+        prevPageText: '上一页',
+        nextPageText: '下一页',
+        description: '基于孤岛架构的 SSG 框架',
+        editLink: {
+          pattern: 'https://github.com/sanyuan0704/island.js/tree/master/docs/:path',
+          text: '📝 在 GitHub 上编辑此页',
+        },
+      },
+    },
+    nav: [
+      {
+        text: 'Home',
+        link: '/',
+        activeMatch: '^/$|^/',
+      },
+    ],
+    sidebar: [
+      {
+        text: 'Test',
+        items: [
+          {
+            text: 'Getting Started',
+            link: 'zh/test/haha',
+          },
+        ],
+      },
+    ],
+  },
+})
---
+++ docs/index.md
@@ -0,0 +1,33 @@
+---
+pageType: home
+
+hero:
+  name: Liuqh's Note
+  text: 记录工作中的东西
+  tagline: 简单、强大、高性能的现代化 SSG 方案
+  image:
+    src: /island.png
+    alt: Island
+  actions:
+    - theme: brand
+      text: 快速开始
+      link: /foo/
+    - theme: alt
+      text: GitHub 地址
+      link: https://github.com/liuqh0609
+
+features:
+  - title: 'Vite: 极速的开发响应速度'
+    details: 基于 Vite 构建，开发时的响应速度极快，即时的热更新，带给你极致的开发体验。
+    icon: 🚀
+  - title: 'MDX: Markdown & React 组件来写内容'
+    details: MDX 是一种强大的方式来写内容。你可以在 Markdown 中使用 React 组件。
+    icon: 📦
+  - title: '孤岛架构: 更高的生产性能'
+    details: 采用 Islands 架构，意味着更少的 JavaScript 代码、局部 hydration， 从而带来更好的首屏性能。
+    icon: ✨
+---
+
+# yes
+
+no
